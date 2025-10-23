df7e97cdb78be296cdacb7519e69f3cb321dd86e
Mary Vonne Diallo <jesuisetudiant10@gmail.com>
2025-10-23 20:20:26 +0000

    Fix Swagger configuration and regenerate docs

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -16,6 +16,47 @@ return [
             ],
             'paths' => [
                 /*
+                 * Absolute path to location where parsed annotations will be stored
+                 */
+                'docs' => storage_path('api-docs'),
+                /*
+                 * Absolute path to directory where to export views
+                 */
+                'views' => base_path('resources/views/vendor/l5-swagger'),
+                /*
+                 * Edit to set the api's base path
+                 */
+                'base' => env('L5_SWAGGER_BASE_PATH', null),
+                /*
+                 * Edit to include full URL in ui for assets
+                 */
+                'use_absolute_path' => env('L5_SWAGGER_USE_ABSOLUTE_PATH', true),
+                /*
+                * Edit to set path where swagger ui assets should be stored
+                */
+                'swagger_ui_assets_path' => env('L5_SWAGGER_UI_ASSETS_PATH', 'vendor/swagger-api/swagger-ui/dist/'),
+                /*
+                 * File name of the generated json documentation file
+                 */
+                'docs_json' => 'api-docs.json',
+                /*
+                 * File name of the generated YAML documentation file
+                 */
+                'docs_yaml' => 'api-docs.yaml',
+                /*
+                 * Set this to `json` or `yaml` to determine which documentation file to use in UI
+                 */
+                'format_to_use_for_docs' => env('L5_FORMAT_TO_USE_FOR_DOCS', 'json'),
+                /*
+                 * Absolute paths to directory containing the swagger annotations are stored.
+                 */
+                'annotations' => [
+                    base_path('app/Swagger'),
+                    base_path('app/Http/Controllers'),
+                ],
+            ],
+            'paths' => [
+                /*
                  * Edit to include full URL in ui for assets
                  */
                 'use_absolute_path' => env('L5_SWAGGER_USE_ABSOLUTE_PATH', false),
@@ -56,6 +97,10 @@ return [
              * Route for accessing parsed swagger annotations.
              */
             'docs' => 'docs',
+            /*
+             * Route for accessing api documentation interface
+             */
+            'api' => 'api/documentation',
 
             /*
              * Route for Oauth2 authentication callback.
