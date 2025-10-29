c726402205f344b6055f7e44010576f4ddd52913
Mary Vonne Diallo <jesuisetudiant10@gmail.com>
2025-10-29 07:10:27 +0000

    modifie dmary10

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,6 +40,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\Http\Middleware\HandleCors::class,
             \App\Http\Middleware\ApiHeadersMiddleware::class,
             \App\Http\Middleware\ThrottleMiddleware::class,
             \App\Http\Middleware\LoggingMiddleware::class,
