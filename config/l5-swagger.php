ee665cc73582b9d6a9ea21030e8422623d7f8cba
Mary Vonne Diallo <jesuisetudiant10@gmail.com>
2025-10-29 20:12:46 +0000

    bobo6

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -14,6 +14,7 @@ return [
                 /*
                  * Route for accessing api documentation interface
                  */
+                'docs' => 'maryvonne/docs',
                 'api' => 'maryvonne/documentation',
             ],
             'paths' => [
