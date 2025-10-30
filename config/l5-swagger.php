6242c27d225137c7c29bf3d230311dd845677c64
Mary Vonne Diallo <jesuisetudiant10@gmail.com>
2025-10-30 13:09:46 +0000

    localfatttt

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -88,7 +88,7 @@ return [
             /*
              * Absolute path to location where parsed annotations will be stored
              */
-            'docs' => public_path('api-docs'),
+            'docs' => storage_path('api-docs'),
 
             /*
              * Absolute path to directory where to export views
